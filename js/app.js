09a0c7a7461d83db0aee6a7f569bffa743d511e9
Leandro Nazar <ljnazar@gmail.com>
2022-08-27 20:18:03 -0300

    add_fixed_speed_animation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
-let tableArrowsFixed = document.getElementById("table-arrows-fixed");
-let tableArrowsSong = document.getElementById("table-arrows-song");
+let tableFixed = document.getElementById("table-arrows-fixed");
+let tableSong = document.getElementById("table-arrows-song");
 
-/*console.log(tableArrowsFixed.offsetWidth);
-console.log(tableArrowsFixed.offsetHeight);
-console.log(tableArrowsSong.offsetWidth);
-console.log(tableArrowsSong.offsetHeight);*/
-
-let asd = "-" + tableArrowsSong.offsetHeight + "px";
 let rootElement = document.documentElement;
-rootElement.style.setProperty("--asd", asd);
+
+/* Code to determine variable table height */
+let tableHeight = "-" + tableSong.offsetHeight + "px";
+rootElement.style.setProperty("--tableHeight", tableHeight);
+
+/* Code to calculate variable duration of translateY */
+/* Allows to give a fixed speed to the animation */
+let totalPageHeight = document.body.scrollHeight;
+let speed = 160;
+let duration = `${totalPageHeight / speed}s`;
+rootElement.style.setProperty("--duration", duration);
+console.log(duration);
+
+/* Code to determine position of element HTML */
+let positionTableFixedTop = tableFixed.offsetTop;
+console.log(positionTableFixedTop);
+/*let positionTableSongTop = tableSong.offsetTop;
+console.log(positionTableSongTop);*/
+/*let positionTableSong = tableSong.getBoundingClientRect();
+console.log("x: "+ positionTableSong.x);*/
+
+
+
+/* Test */
+/*window.addEventListener('load', function(){
+let i = 0;
+for(i=0;i<15000;i++){
+    console.log(tableSong.getBoundingClientRect().top);
+}
+});*/
+/*console.log(tableSong.getBoundingClientRect().top);*/
