0097540c1ac8164251a1aa47fc1fc5804d349dab
Leandro Nazar <ljnazar@gmail.com>
2022-08-29 18:07:44 -0300

    change_animation-to-intersection-observer

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@ rootElement.style.setProperty("--duration", duration);
 console.log(duration);
 
 /* Code to determine position of element HTML */
-let positionTableFixedTop = tableFixed.offsetTop;
-console.log(positionTableFixedTop);
+/*let positionTableFixedTop = tableFixed.offsetTop;
+console.log(positionTableFixedTop);*/
+
 /*let positionTableSongTop = tableSong.offsetTop;
 console.log(positionTableSongTop);*/
 /*let positionTableSong = tableSong.getBoundingClientRect();
@@ -35,17 +36,63 @@ for(i=0;i<15000;i++){
 /*console.log(tableSong.getBoundingClientRect().top);*/
 
 /* Test 2 */
-function callback(){
-    tableSong.style.border = "1px solid";
-    console.log("holis");
-}
+/*
+const callback = (entries) => {
+    entries.map((entry) => {
+        console.log("Hello world!");
+    });
+};
+
+let arrow1 = document.getElementById("arrow-1");
+let arrow2 = document.getElementById("arrow-2");
 
 let options = {
-    root: tableFixed,
+    root: arrow1,
     rootMargin: "0px",
     threshold: 1.0
 }
 
 let observer = new IntersectionObserver(callback, options);
 
-observer.observe(tableSong);
+observer.observe(arrow2);*/
+
+/* Test 3 */
+
+const spyNav = document.getElementById("spy-nav");
+const arrows = [...document.querySelectorAll("td > img")];
+
+const spyItem = (entries, observer) => {
+  entries.forEach((entry) => {
+    const { id } = entry.target;
+    const spy = spyNav.querySelector(`[href="#${id}"`);
+
+    spy.classList.remove("active");
+    if (!entry.isIntersecting) return;
+    spy.classList.add("active");
+  });
+};
+
+const observer = new IntersectionObserver(spyItem, {
+  rootMargin: "-5% 0% -95% 0%",
+  root: document, // <- This is only here to force the iframe document.
+});
+
+arrows.forEach((arrow) => observer.observe(arrow));
+
+/*
+() => {
+  console.log("prueba");
+  window.scrollBy(0,1);
+  scrolldelay = setTimeout(pageScroll,10);
+}*/
+
+function pageScroll() {
+  window.scrollBy(0,1);
+  scrolldelay = setTimeout(pageScroll,4);
+}
+pageScroll();
+
+
+
+
+
