21462ee406a9325f941b0db34901f36c184de29a
Leandro Nazar <ljnazar@gmail.com>
2022-08-29 01:55:40 -0300

    test_intersection-observer

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ rootElement.style.setProperty("--tableHeight", tableHeight);
 /* Code to calculate variable duration of translateY */
 /* Allows to give a fixed speed to the animation */
 let totalPageHeight = document.body.scrollHeight;
-let speed = 160;
+let speed = 200;
 let duration = `${totalPageHeight / speed}s`;
 rootElement.style.setProperty("--duration", duration);
 console.log(duration);
@@ -25,7 +25,7 @@ console.log("x: "+ positionTableSong.x);*/
 
 
 
-/* Test */
+/* Test 1 */
 /*window.addEventListener('load', function(){
 let i = 0;
 for(i=0;i<15000;i++){
@@ -33,3 +33,19 @@ for(i=0;i<15000;i++){
 }
 });*/
 /*console.log(tableSong.getBoundingClientRect().top);*/
+
+/* Test 2 */
+function callback(){
+    tableSong.style.border = "1px solid";
+    console.log("holis");
+}
+
+let options = {
+    root: tableFixed,
+    rootMargin: "0px",
+    threshold: 1.0
+}
+
+let observer = new IntersectionObserver(callback, options);
+
+observer.observe(tableSong);
